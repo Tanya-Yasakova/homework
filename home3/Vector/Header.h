faaf21e170be65166b15328d5d19bfca72e3abaf
unknown <ITMM-230042@Q113.local>
2023-09-27 11:00:46 +0300

    f

--- home3/Vector/Header.h
+++ home3/Vector/Header.h
@@ -1,141 +1,170 @@
-#ifndef Header_h
-#define Header_h
-#include <iostream>
-#include "math.h"
-using namespace std;
+#ifndef HEADER_H
+#define HEADER_H
 
-template <class T>
+
+#include <iostream>
+#include <math.h>
+
+template <typename T>
 class Vector {
 private:
-	T* vec;
-	int size;
+	int n;
+	T* coor;
 public:
-	Vector(int size_ = 10);
+	Vector();
+	Vector(int size);
 	Vector(const Vector<T>& obj);
 	~Vector();
-	const Vector<T>& operator=(const Vector<T>& obj);
-	Vector<T> operator+(const Vector<T>& obj);
-	Vector<T> operator-(const Vector<T>& obj);
-	double operator*(const Vector<T>& obj) const;
-	bool operator==(const Vector<T>& obj) const;
-	bool operator!=(const Vector <T>& obj) const;
+	Vector <T> operator + (const Vector<T>& obj);
+	Vector <T> operator - (const Vector<T>& obj);
+	double operator * (const Vector<T>& obj);
 	double length() const;
-	
-	friend ostream& operator<<(ostream& out, const Vector<T>& obj) {
-		out << "(" << obj.vec[0] << ", ";
-		for (int i = 1; i < obj.size - 1; i++) {
-			out << obj.vec[i] << ", ";
+	bool operator == (const Vector<T>& obj) const;
+	bool operator != (const Vector<T>& obj) const;
+	const Vector<T>& operator = (const Vector<T>& obj);
+	friend std::ostream& operator << (std::ostream& stream, const Vector<T>& obj) {
+		stream << '{';
+		for (int i = 0; i < obj.n; i++) {
+			stream << obj.coor[i] << ",";
 		}
-		out << obj.vec[obj.size-1] << ") ";
-		return out;
-	}
+		stream << '}' << std::endl;;
+		return stream;
 
-	friend istream& operator>>(istream& in, Vector<T>& obj) {
-		for (int i = 0; i < obj.size; i++) {
-			in >> obj.vec[i];
+	}
+	friend std::istream& operator >> (std::istream& stream, Vector<T>& obj) {
+		for (int i = 0; i < obj.n; i++) {
+			stream >> obj.coor[i];
 		}
-		return in;
+		return stream;
 	}
 };
 
 template <typename T>
-Vector <T>::Vector(int size_) {
-	if (size_ <= 0)
-		throw "size error";
-	size = size_;
-	vec = new T[size];
+Vector<T>::Vector() {
+	n = 0;
+	coor = new T[0];
 }
 
 template <typename T>
-Vector <T>::Vector(const Vector <T>& obj) {
-	size = obj.size;
-	vec = new T[size];
-	for (int i = 0; i < size; i++) {
-		vec[i] = obj.vec[i];
+Vector<T>::Vector(int size) {
+	n = size;
+	coor = new T[size];
+}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& obj) {
+	n = obj.n;
+	coor = new T[n];
+	for (int i = 0; i < n; i++) {
+		coor[i] = obj.coor[i];
 	}
 }
 
+
 template <typename T>
-Vector <T>::~Vector() {
-	if (size > 0)
-		delete[] vec;
+Vector<T>::~Vector() {
+	n = 0;
+	delete[] coor;
 }
 
 template <typename T>
-const Vector<T>& Vector<T>::operator=(const Vector <T>& obj) {
-	if (this == &(obj))
-		return (*this);
-	if (size != obj.size) {
-		delete[] vec;
-		size = obj.size;
-		vec = new T[size];
-	}
-	for (int i = 0; i < size; i++) {
-		vec[i] = obj.vec[i];
-	}
-	return (*this);
-}
-
-template <typename T>
-Vector <T> Vector <T>::operator + (const Vector <T>& obj) {
-	if (size != obj.size) {
+Vector<T> Vector<T>:: operator + (const Vector<T>& obj) {
+	if (n != obj.n) {
 		throw "Diferent size";
 	}
-	Vector <T> tmp(size);
-	for (int i = 0; i < size; i++) {
-		tmp.vec[i] += obj.vec[i];
+	Vector<T> tmp(*this);
+	for (int i = 0; i < n; i++) {
+		tmp.coor[i] += obj.coor[i];
 	}
 	return tmp;
 }
 
 template <typename T>
-Vector <T> Vector <T>::operator - (const Vector <T>& obj) {
-	if (size != obj.size) {
-		throw "Diferent spaces";
+Vector <T> Vector<T>:: operator - (const Vector<T>& obj) {
+	if (n != obj.n) {
+		throw "Diferent size";
 	}
-	Vector <T> tmp(size);
-	for (int i = 0; i < size; i++) {
-		tmp.vec[i] -= obj.vec[i];
+	Vector<T> tmp(*this);
+	for (int i = 0; i < n; i++) {
+		tmp.coor[i] -= obj.coor[i];
 	}
 	return tmp;
 }
 
-template <typename T>
-double Vector <T>::operator * (const Vector <T>& obj) const {
-	if (size != obj.size) {
-		throw "Diferent spaces";
-	}
-	T answer = 0;
-	for (int i = 0; i < size; i++) {
-		answer += vec[i] * obj.vec[i];
-	}
-	return (double)answer;
-}
+
 
 template <typename T>
-bool Vector <T>:: operator==(const Vector<T>& obj) const {
-	if (this == &obj)
-		return true;
-	if (size != obj.size)
-		return false;
-	for (int i = 0; i < size; i++) {
-		if (vec[i] != obj.vec[i])
+double Vector<T>:: operator * (const Vector<T>& obj) {
+	if (n != obj.n) {
+		throw "Diferent size";
+	}
+	Vector<T> tmp(*this);
+	double res = 0.0;
+	for (int i = 0; i < n; i++) {
+		res += tmp.coor[i] * obj.coor[i];
+	}
+	return res;
+}
+
+
+
+template <typename T>
+double Vector<T>::length() const {
+	double res = 0.0;
+	for (int i = 0; i < n; i++) {
+		res += coor[i] * coor[i];
+	}
+	return (sqrt(res));
+}
+
+
+template <typename T>
+bool Vector<T>:: operator == (const Vector<T>& obj) const {
+	if (n != obj.n) {
+		throw "Diferent size";
+	}
+	for (int i = 0; i < n; i++) {
+		if (coor[i] != obj.coor[i]) {
 			return false;
+		}
 	}
 	return true;
 }
 
 template <typename T>
-bool Vector<T>::operator!=(const Vector <T>& obj) const {
-	return !((*this) == obj);
+bool Vector<T>::operator != (const Vector<T>& obj) const {
+	if (n != obj.n) {
+		throw "Diferent size";
+	}
+	for (int i = 0; i < n; i++) {
+		if (coor[i] != obj.coor[i]) {
+			return true;
+		}
+	}
+	return false;
 }
 
+
+
 template <typename T>
-double Vector<T>::length() const {
-	double len = 0.f;
-	for (int i = 0; i < size; i++) {
-		len = len + vec[i] * vec[i];
+const Vector<T>& Vector<T>::operator = (const Vector<T>& obj) {
+	if (this == &obj) {
+		return(*this);
 	}
-	return sqrt((double)len);
+	if (n != obj.n) {
+		delete[] coor;
+		n = obj.n;
+		coor = new T[n];
+	}
+
+	for (int i = 0; i < n; i++) {
+		coor[i] = obj.coor[i];
+	}
+
+	return (*this);
 }
+
+
+
+
 #endif
